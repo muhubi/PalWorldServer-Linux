925f78ba0f277d9a4af66a3cbba0e99dcc8043ac
Andrew Mahabee <andrew.mahabee@proton.me>
2024-02-19 21:09:52 -0500

    Update PalServer_update_run.sh
    formatting and added descriptor for +app_update
    
    Signed-off-by: Andrew Mahabee <andrew.mahabee@proton.me>

--- Shell Scripts/PalServer_update_run.sh
+++ Shell Scripts/PalServer_update_run.sh
@@ -4,13 +4,16 @@
 # PURPOSE: This script when ran will update the PalServer application using SteamCMD, validate the application integrity, and then start the PalServer application.
 
 # The following command updates the PalServer application from Steam using an anonymous account.
+
 # Command breakdown: 
 # [steamcmd: application being ran] 
 # [+login anonymous: logs into Steam anonymously, PalServer does not require a separate Steam account]
+# [+app_update: this command updates the selected Steam application]
 # [2394010: this is the SteamID for the PalServer application]
 # [validate: this forces steamcmd to validate the PalServer application, optional but recommended]
 # [+quit: this exits steamcmd once the update and validation is complete]
 steamcmd +login anonymous +app_update 2394010 validate +quit
+
 
 # This command starts the PalServer application in the background, assuming steamcmd was used to install PalServer in the current user's account
 ~/Steam/steamapps/common/PalServer/PalServer.sh
